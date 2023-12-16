d5b31adc2c473c1eb1e7384a2fb7ec6ff8a77bac
A. Urquiola <patchamama.go@gmail.com>
2023-12-16 02:33:24 +0100

    add express server

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+// Heroku dynamically sets a port
+const PORT = process.env.PORT || 5000
+
+app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+app.get('/version', (req, res) => {
+  res.send('0.2')
+})
+
+app.listen(PORT, () => {
+  console.log('server started on port ', PORT)
+})
